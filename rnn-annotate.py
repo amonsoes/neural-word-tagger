6fe7ec756c4f9b8aadc86826be0d0c88e15c9627
soaresdesouza <amonsoares@Amons-MacBook-Pro.local>
2020-09-23 11:00:14 +0200

    adds generator to rnn-annontate

--- rnn-annotate.py
+++ rnn-annotate.py
@@ -13,10 +13,13 @@ def annotate(path, data, tagger):
     sents_generator = data.sentences(path)
     for sent in sents_generator:
         output = tagger(torch.LongTensor(data.words2IDs(sent)))
-    yield data.IDs2tags(output)
+        yield (sent,data.IDs2tags(output))
         
 if __name__ == '__main__':
     
     data = tg.Data(args.path_param+'.io')
     model = torch.load(args.path_param+'.rnn')
     output_generator = annotate(args.path_sents, data, model)
+    for sent, output in output_generator:
+        for i in range(0,len(sent)):
+            print(sent[i],output[i])
